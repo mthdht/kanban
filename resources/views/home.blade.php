6b72f12d0d9e9b3c1943265056aa1113a1f8e516
mthdht <mthdht@gmail.com>
2018-01-23 11:39:52 +0100

    add recent project section layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <div class="topBoard w3-col m11 w3-row-padding">
+    <div class="topBoard w3-row-padding">
         <!-- Header -->
         <header class="w3-container" style="padding-top:22px">
             <h5><b><i class="fa fa-dashboard"></i> My Dashboard</b></h5>
@@ -53,11 +53,59 @@
         </div>
     </div>
 
-    <div class="recent">
+    <div class="recent w3-margin-top w3-row-padding">
+        <!-- Header -->
+        <header class="w3-container" style="padding-top:22px">
+            <h5><b><i class="fa fa-folder"></i> Projets récents</b></h5>
+        </header>
 
+        <div class="w3-quarter">
+            <div class="w3-container w3-padding-16">
+                <h4 class="w3-margin-bottom">projet 1</h4>
+                <div class="w3-left"><i class="fa fa-file-code-o w3-xxxlarge"></i></div>
+                <div class="w3-right">
+                    <h3>6</h3>
+                </div>
+                <div class="w3-clear"></div>
+                <div class="tag w3-margin-top">js-php</div>
+            </div>
+        </div>
+        <div class="w3-quarter">
+            <div class="w3-container w3-padding-16">
+                <h4 class="w3-margin-bottom">projet 1</h4>
+                <div class="w3-left"><i class="fa fa-file-code-o w3-xxxlarge"></i></div>
+                <div class="w3-right">
+                    <h3>6</h3>
+                </div>
+                <div class="w3-clear"></div>
+                <div class="tag w3-margin-top">js-php</div>
+            </div>
+        </div>
+        <div class="w3-quarter">
+            <div class="w3-container w3-padding-16">
+                <h4 class="w3-margin-bottom">projet 1</h4>
+                <div class="w3-left"><i class="fa fa-file-code-o w3-xxxlarge"></i></div>
+                <div class="w3-right">
+                    <h3>6</h3>
+                </div>
+                <div class="w3-clear"></div>
+                <div class="tag w3-margin-top">js-php</div>
+            </div>
+        </div>
+        <div class="w3-quarter">
+            <div class="w3-container w3-padding-16">
+                <h4 class="w3-margin-bottom">projet 1</h4>
+                <div class="w3-left"><i class="fa fa-file-code-o w3-xxxlarge"></i></div>
+                <div class="w3-right">
+                    <h3>6</h3>
+                </div>
+                <div class="w3-clear"></div>
+                <div class="tag w3-margin-top">js-php</div>
+            </div>
+        </div>
     </div>
 
-    <div class="stat">
+    <div class="stat w3-row-padding">
 
     </div>
 @endsection
