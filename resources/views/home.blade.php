5ac9c8ecb1f4fd3c9f4888a58cf1f75d0ee8cecf
mthdht <mthdht@gmail.com>
2018-01-23 12:27:13 +0100

    finish layout for recent project section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,7 +26,7 @@
                     <h3>52</h3>
                 </div>
                 <div class="w3-clear"></div>
-                <h4>Taches</h4>
+                <h4>Tickets</h4>
             </div>
         </div>
 
@@ -60,52 +60,64 @@
         </header>
 
         <div class="w3-quarter">
-            <div class="w3-container w3-padding-16">
-                <h4 class="w3-margin-bottom">projet 1</h4>
-                <div class="w3-left"><i class="fa fa-file-code-o w3-xxxlarge"></i></div>
+            <div class="w3-container w3-padding-16 w3-center w3-round w3-card w3-hover-text-teal">
+                <h3 class="w3-margin-bottom"><b>projet 1</b></h3>
+                <div class="w3-left"><i class="fa fa-file-code-o w3-xxlarge w3-margin"></i></div>
                 <div class="w3-right">
                     <h3>6</h3>
+                    <p> Tickets</p>
                 </div>
                 <div class="w3-clear"></div>
-                <div class="tag w3-margin-top">js-php</div>
+                <div class="tag"><i class="fa fa-tags"></i> js-php</div>
             </div>
         </div>
+
         <div class="w3-quarter">
-            <div class="w3-container w3-padding-16">
-                <h4 class="w3-margin-bottom">projet 1</h4>
-                <div class="w3-left"><i class="fa fa-file-code-o w3-xxxlarge"></i></div>
+            <div class="w3-container w3-padding-16 w3-center w3-round w3-card w3-hover-text-teal">
+                <h3 class="w3-margin-bottom"><b>projet 1</b></h3>
+                <div class="w3-left"><i class="fa fa-file-code-o w3-xxlarge w3-margin"></i></div>
                 <div class="w3-right">
                     <h3>6</h3>
+                    <p> Tickets</p>
                 </div>
                 <div class="w3-clear"></div>
-                <div class="tag w3-margin-top">js-php</div>
+                <div class="tag"><i class="fa fa-tags"></i> js-php</div>
             </div>
         </div>
+
         <div class="w3-quarter">
-            <div class="w3-container w3-padding-16">
-                <h4 class="w3-margin-bottom">projet 1</h4>
-                <div class="w3-left"><i class="fa fa-file-code-o w3-xxxlarge"></i></div>
+            <div class="w3-container w3-padding-16 w3-center w3-round w3-card w3-hover-text-teal">
+                <h3 class="w3-margin-bottom"><b>projet 1</b></h3>
+                <div class="w3-left"><i class="fa fa-file-code-o w3-xxlarge w3-margin"></i></div>
                 <div class="w3-right">
                     <h3>6</h3>
+                    <p> Tickets</p>
                 </div>
                 <div class="w3-clear"></div>
-                <div class="tag w3-margin-top">js-php</div>
+                <div class="tag"><i class="fa fa-tags"></i> js-php</div>
             </div>
         </div>
+
         <div class="w3-quarter">
-            <div class="w3-container w3-padding-16">
-                <h4 class="w3-margin-bottom">projet 1</h4>
-                <div class="w3-left"><i class="fa fa-file-code-o w3-xxxlarge"></i></div>
+            <div class="w3-container w3-padding-16 w3-center w3-round w3-card w3-hover-text-teal">
+                <h3 class="w3-margin-bottom"><b>projet 1</b></h3>
+                <div class="w3-left"><i class="fa fa-file-code-o w3-xxlarge w3-margin"></i></div>
                 <div class="w3-right">
                     <h3>6</h3>
+                    <p> Tickets</p>
                 </div>
                 <div class="w3-clear"></div>
-                <div class="tag w3-margin-top">js-php</div>
+                <div class="tag"><i class="fa fa-tags"></i> js-php</div>
             </div>
         </div>
+
+
     </div>
 
     <div class="stat w3-row-padding">
-
+        <!-- Header -->
+        <header class="w3-container" style="padding-top:22px">
+            <h5><b><i class="fa fa-area-chart"></i> Statistics</b></h5>
+        </header>
     </div>
 @endsection
