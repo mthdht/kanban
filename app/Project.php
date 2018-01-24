a551584d55ebc682047102f1b3a98b92983b7424
mthdht <mthdht@gmail.com>
2018-01-24 19:33:47 +0100

    add relation to category and task model

--- app/Project.php
+++ app/Project.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
@@ -12,4 +13,24 @@ class Project extends Model
      * @var array
      */
     protected $guarded = [];
+
+    /**
+     * Get the project's categories.
+     *
+     * @return Category Collection
+     */
+    public function categories()
+    {
+        return $this->hasMany('App\Category');
+    }
+
+    /**
+     * get all the project's tasks.
+     *
+     * @return Task Collection
+     */
+    public function tasks()
+    {
+        return $this->hasManyThrough('App\Task', 'App\Category');
+    }
 }
