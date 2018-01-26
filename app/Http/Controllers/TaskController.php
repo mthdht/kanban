6c0cb470773e4a1532e98a94c2040b07a723fbfb
mthdht <mthdht@gmail.com>
2018-01-26 19:39:18 +0100

    made indexx method on task controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -24,7 +24,8 @@ class TaskController extends Controller
      */
     public function index()
     {
-        //
+        $tasks = Task::all();
+        return view('tasks.index', ['tasks' => $tasks]);
     }
 
     /**
